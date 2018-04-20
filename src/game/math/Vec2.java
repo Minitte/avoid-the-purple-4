a26886cdf41f5d52dbd96f52702840d2e496bddc
Minitte <arcentalminitte@gmail.com>
2018-04-20 16:18:24 -0700

    more vec2 operations

--- src/game/math/Vec2.java
+++ src/game/math/Vec2.java
@@ -26,5 +26,49 @@ public class Vec2 {
 		this.x = x;
 		this.y = y;
 	}
+	
+	/**
+	 * Adds amt to this vector
+	 * @param amt
+	 */
+	public void add(Vec2 amt) {
+		x += amt.x;
+		y += amt.y;
+	}
+	
+	/**
+	 * Minus amt to this vector
+	 * @param amt
+	 */
+	public void minus(Vec2 amt) {
+		x += amt.x;
+		y += amt.y;
+	}
+	
+	/**
+	 * Multiply amt to this vector
+	 * @param amt
+	 */
+	public void multiply(Vec2 amt) {
+		x *= amt.x;
+		y *= amt.y;
+	}
+	
+	/**
+	 * Divide amt to this vector
+	 * @param amt
+	 */
+	public void divide(Vec2 amt) {
+		x /= amt.x;
+		y /= amt.y;
+	}
+	
+	/**
+	 * Dot product with amt
+	 * @param amt
+	 */
+	public float dot(Vec2 amt) {
+		return (x * amt.y) + (y * amt.x);
+	}
 
 }
